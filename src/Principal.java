28c47fc28d5bd01445e18f384be269f32bcef99d
venancio2000 <jbrunogvenancio@gmail.com>
2024-05-15 22:32:48 -0300

    Documentacao java

--- src/Principal.java
+++ src/Principal.java
@@ -1,5 +1,18 @@
-public class Main {
+public class Principal {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Filme meuFilme = new Filme();
+        meuFilme.nome = "O poderoso chefão";
+        meuFilme.anoDeLancamento = 1970;
+        meuFilme.duracaoEmMinutos = 188;
+
+        meuFilme.exibeFichaTecnica();
+        meuFilme.avalia(8);
+        meuFilme.avalia(5);
+        meuFilme.avalia(10);
+
+        System.out.println("Total de avaliações: " + meuFilme.getTotalDeAvaliacoes());
+        System.out.println(meuFilme.pegaMedia());
+
+
     }
 }
